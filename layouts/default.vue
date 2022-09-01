250981a4d2acc3bc04d5ddecaf307fb935a802da
jylee-idv <108310917+jylee-idv@users.noreply.github.com>
2022-09-02 00:56:41 +0900

    검색 UI 및 검색 기능 구현

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <h1>
+        <nuxt-link to="/">Nuxt Shopping</nuxt-link>
+      </h1>
+    </header>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,84 @@
 <template>
-  <Tutorial />
+  <div class="app">   
+    <main>
+      <search-input v-model="searchKeyword"></search-input>
+      <ul>
+        <li class="item flex" v-for="product in products" :key="product.id" @click="moveToDetailPage(product.id)">          
+          <img class="product-image" :src="product.imageUrl" :alt="product.name">
+          <p>{{product.name}}</p>
+          <span>{{product.price}}</span>
+        </li>
+      </ul>
+    </main>    
+  </div>  
 </template>
 
 <script>
+import axios from 'axios'
+import SearchInput from '@/components/SearchInput.vue';
+
 export default {
-  name: 'IndexPage',
+  components: { SearchInput },  
+  async asyncData(){
+    const response = await axios.get('http://localhost:3000/products')
+    console.log(response);
+    const products = response.data.map((item) => {
+      return{
+        ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`  
+      }      
+    })
+    return { products }
+  },
+
+  data(){
+    return{
+      searchKeyword: '',
+    }
+  },
+
+  methods: {
+    moveToDetailPage(id){
+      console.log(id);
+      this.$router.push(`detail/${id}`)
+    },
+    updateSearchKeyword(keyword){
+      this.searchKeyword = keyword
+    }
+  }
 }
 </script>
+
+<style scoped>
+  .flex {
+    display: flex;
+    justify-content: center;
+  }
+  .item {
+    display: inline-block;
+    width: 400px;
+    height: 300px;
+    text-align: center;
+    margin: 0 0.5rem;
+    cursor: pointer;
+  }
+  .product-image {
+    width: 400px;
+    height: 250px;
+  }
+  .app {
+    position: relative;
+  }
+  .cart-wrapper {
+    position: sticky;
+    float: right;
+    bottom: 50px;
+    right: 50px;
+  }
+  .cart-wrapper .btn {
+    display: inline-block;
+    height: 40px;
+    font-size: 1rem;
+    font-weight: 500;
+  }
+  </style>
